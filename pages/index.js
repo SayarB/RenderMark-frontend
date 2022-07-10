97f26ae392dc598c9a24c2b4b82aa646e5a24322
SayarB <sayarbhattacharyya03@gmail.com>
2022-07-10 22:03:46 +0530

    fix: yet another standard fix

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { useEffect, useState } from 'react'
 import styles from '../styles/Home.module.css'
 import { marked } from 'marked'
+const DOMParser = window.DOMParser
+const FileReader = window.FileReader
 export default function Home () {
   const [file, setFile] = useState(null)
   const [fileText, setFileText] = useState('')
