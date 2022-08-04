4842ebe16584659aa097b20f8df8455c9a028631
SayarB <sayarbhattacharyya03@gmail.com>
2022-08-04 10:36:19 +0530

    fix: linting fixes

--- components/ImagePreview/ImagePreview.js
+++ components/ImagePreview/ImagePreview.js
@@ -1,26 +1,26 @@
-import styles from "../../styles/ImagePreview.module.css";
-import LinkIcon from "@mui/icons-material/Link";
-import { ButtonBase } from "@mui/material";
+import styles from '../../styles/ImagePreview.module.css'
+import LinkIcon from '@mui/icons-material/Link'
+import { ButtonBase } from '@mui/material'
 
-function ImagePreview({ key, src, onClick }) {
+function ImagePreview ({ key, src, onClick }) {
   return (
-    <div key={key} className={styles["image-preview"]}>
+    <div key={key} className={styles['image-preview']}>
       <img src={src} />
       <div className={styles.overlay}>
-        <ButtonBase onClick={onClick} sx={{ borderRadius: "50%" }}>
+        <ButtonBase onClick={onClick} sx={{ borderRadius: '50%' }}>
           <LinkIcon
             sx={{
-              fontSize: "4rem",
-              color: "white",
-              "&:hover": {
-                fontSize: "4.3rem",
-              },
+              fontSize: '4rem',
+              color: 'white',
+              '&:hover': {
+                fontSize: '4.3rem'
+              }
             }}
           />
         </ButtonBase>
       </div>
     </div>
-  );
+  )
 }
 
-export default ImagePreview;
+export default ImagePreview
