7dbada241cc1a2274a533d04c49cd9baaced42ce
SayarB <sayarbhattacharyya03@gmail.com>
2022-08-13 11:51:52 +0530

    fix: linting fixed

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,30 +1,30 @@
-import styles from "../../styles/Navbar.module.css";
-import Button from "../Button/Button";
-import Link from "next/link";
-import Image from "next/image";
+import styles from '../../styles/Navbar.module.css'
+import Button from '../Button/Button'
+import Link from 'next/link'
+import Image from 'next/image'
 // import useMediaQuery from "@mui/material/useMediaQuery";
-import { useRouter } from "next/router";
+import { useRouter } from 'next/router'
 
-export default function Navbar({ selected }) {
-  const router = useRouter();
+export default function Navbar ({ selected }) {
+  const router = useRouter()
   // const isMobile = useMediaQuery("(max-width:756px)");
   return (
     <div
       className={styles.navbar}
       style={{
-        backgroundColor: selected === "/templates" ? "white" : "transparent",
+        backgroundColor: selected === '/templates' ? 'white' : 'transparent'
       }}
     >
-      <div className={styles["nav-left"]}>
-        <Link href="/">
-          <div className={styles["logo-image"]} style={{ cursor: "pointer" }}>
+      <div className={styles['nav-left']}>
+        <Link href='/'>
+          <div className={styles['logo-image']} style={{ cursor: 'pointer' }}>
             <Image
-              layout="fixed"
-              alt="Logo"
+              layout='fixed'
+              alt='Logo'
               src={
-                selected !== "/templates" && selected !== "/template/[id]/edit"
-                  ? "/logo.svg"
-                  : "/logo_black.svg"
+                selected !== '/templates' && selected !== '/template/[id]/edit'
+                  ? '/logo.svg'
+                  : '/logo_black.svg'
               }
               width={200}
               height={80}
@@ -33,13 +33,13 @@ export default function Navbar({ selected }) {
         </Link>
       </div>
 
-      <div className={styles["nav-right"]}>
-        {selected !== "/templates" && (
+      <div className={styles['nav-right']}>
+        {selected !== '/templates' && (
           <Button
-            onClick={() => router.push("/templates")}
+            onClick={() => router.push('/templates')}
             style={{
-              background: "linear-gradient(90deg, #12C2E9 0%, #C471ED 150%)",
-              color: "white",
+              background: 'linear-gradient(90deg, #12C2E9 0%, #C471ED 150%)',
+              color: 'white'
             }}
           >
             Choose A Template
@@ -47,5 +47,5 @@ export default function Navbar({ selected }) {
         )}
       </div>
     </div>
-  );
+  )
 }
