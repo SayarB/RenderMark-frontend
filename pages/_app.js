0c6ddd99209ca1ccfe5a6541ced158df730257d1
SayarB <sayarbhattacharyya03@gmail.com>
2022-08-14 18:43:09 +0530

    feat: made with love by gdsc vit added to footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,63 +1,71 @@
-import '../styles/globals.css'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import 'react-toastify/dist/ReactToastify.css'
-import { ToastContainer } from 'react-toastify'
-import Navbar from '../components/Navbar/Navbar'
-import { useRouter } from 'next/router'
-import Link from 'next/link'
-import { useEffect, useState } from 'react'
+import "../styles/globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+import Navbar from "../components/Navbar/Navbar";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import Footer from "../components/Footer/Footer";
 
-const queryClient = new QueryClient()
-function MyApp ({ Component, pageProps }) {
-  const router = useRouter()
-  console.log(router.pathname)
-  const [currentVideo, setCurrentVideo] = useState('')
+const queryClient = new QueryClient();
+function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  console.log(router.pathname);
+  const [currentVideo, setCurrentVideo] = useState("");
 
   useEffect(() => {
-    function getWithExpiry (key) {
-      const itemStr = window.localStorage.getItem(key)
+    function getWithExpiry(key) {
+      const itemStr = window.localStorage.getItem(key);
       // if the item doesn't exist, return null
       if (!itemStr) {
-        return null
+        return null;
       }
-      const item = JSON.parse(itemStr)
-      const now = new Date()
+      const item = JSON.parse(itemStr);
+      const now = new Date();
       // compare the expiry time of the item with the current time
       if (now.getTime() > item.expiry) {
         // If the item is expired, delete the item from storage
         // and return null
-        window.localStorage.removeItem(key)
-        return null
+        window.localStorage.removeItem(key);
+        return null;
       }
-      return item.value
+      return item.value;
     }
 
-    setCurrentVideo(getWithExpiry('currentVideoRenderTaskId'))
-  })
+    setCurrentVideo(getWithExpiry("currentVideoRenderTaskId"));
+  });
 
   return (
     <QueryClientProvider client={queryClient}>
       <Navbar selected={router.pathname} />
       <Component {...pageProps} />
       <ToastContainer
-        theme='colored'
+        theme="colored"
         autoClose={5000}
         closeOnClick
         pauseOnFocusLoss
         pauseOnHover={false}
-        style={{ fontSize: '1.4rem' }}
+        style={{ fontSize: "1.4rem" }}
         limit={1}
       />
 
       {currentVideo &&
         currentVideo.length > 0 &&
-        !router.pathname.includes('/status') && (
-          <div className='video-link'>
+        !router.pathname.includes("/status") && (
+          <div className="video-link">
             <Link href={`/status/${currentVideo}`}>Video Render Status</Link>
           </div>
-      )}
+        )}
+      <Footer
+        color={
+          router.pathname === "/templates" || router.pathname.includes("/edit")
+            ? "black"
+            : "white"
+        }
+      />
     </QueryClientProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
